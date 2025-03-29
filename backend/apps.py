f662e9f97c4a22cfd716c432570f162b969cf035
Devashish Nalapareddy <devashish4785@gmail.com>
2025-03-29 14:06:49 -0500

    second commit for Flask backend

--- backend/apps.py
+++ backend/apps.py
@@ -40,4 +40,6 @@ def chat():
     return jsonify({"response": gemini_response})
 
 if __name__ == '__main__':
+    port = os.getenv("PORT", 5000)
+    app.run(debug=True, host="0.0.0.0", port=port)
     app.run(debug=True)
